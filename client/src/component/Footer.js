e15868599ce2ba26d2d985cf3b6ed717729888cf
eakkarunpoom <eakkarunpoom.ta@gmail.com>
2023-06-26 20:08:10 +0700

    first commit

---
+++ client/src/component/Footer.js
@@ -0,0 +1,12 @@
+import Logo from "../img/logo.png"
+
+const Footer = () => {
+    return(
+        <footer>
+            <img src={Logo} alt="logo"/>
+            <span>Made with sass and ReactJS</span>
+        </footer>
+    )
+}
+
+export default Footer;
